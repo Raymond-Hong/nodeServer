11ad65704d5d728628ff94f052d0ec735939adc0
raymond <honxidan@163.com>
2019-04-10 21:54:53 +0800

    update

--- dao/dbutil.js
+++ dao/dbutil.js
@@ -1,11 +1,9 @@
-let mysql = require('mysql');
+const mysql = require('mysql');
 
-module.exports = ()=>{ 
-    return mysql.createConnection({
+module.exports = ()=>mysql.createConnection({
         host:'127.0.0.1',
         port:'3306',
         user:'raymond',
         password:'raymond',
         database:'raymond'
     });
-};
